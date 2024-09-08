fb1746864d5d2969d1f6ceca32a63e63da28bce0
Alex Dickhans <adickhans@gmail.com>
2024-09-07 20:04:17 -0600

    First try

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 // BEZIER_MP_ASSET(skills);
 BEZIER_MIRRORED_MP_ASSET(test);
 
+vex::aivision::colordesc AIVision1__BLUEBOX(1, 7, 56, 188, 10, 0.2);
+vex::aivision::colordesc AIVision1__REDBOX(2, 238, 44, 125, 10, 0.2);
+vex::aivision::colordesc AIVision1__GREENBOX(3, 49, 139, 81, 21, 0.2);
+// AI Vision Code Descriptions
+vex::aivision AIVision1(1, AIVision1__BLUEBOX, AIVision1__REDBOX, AIVision1__GREENBOX);
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -128,5 +134,17 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	// auto code = vex::aivision::colordesc(1, 40, 136, 65, 50, 0.2);
+	// AIVision1.set(code);
+	AIVision1.colorDetection(true, true);
 
+	while (true) {
+		// Take a snapshot to check for detected objects
+		AIVision1.takeSnapshot(AIVision1__GREENBOX);
+
+		// Print how many objects were detected
+		std::cout << "object count: " << AIVision1.objectCount << std::endl;
+
+		pros::Task::delay(10);
+	}
 }
