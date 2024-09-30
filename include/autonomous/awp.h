cb9909e51030dd2bb649a69eecfb0e0d361a3233
Alex Dickhans <adickhans@gmail.com>
2024-09-30 09:30:23 -0600

    redo awp pathing

--- include/autonomous/awp.h
+++ include/autonomous/awp.h
@@ -20,16 +20,8 @@ public:
                 (new Rotate(drivetrain, -90_deg, false, -2000, false))->withTimeout(1.0_s),
                 new TankMotionProfiling(drivetrain, {65_in / second, 100_in / second / second}, 5_in, flip, -90_deg,
                                         0.0),
-                (new Rotate(drivetrain, 50_deg, false, 0.0))->withTimeout(0.8_s),
-                new TankMotionProfiling(drivetrain, {65_in / second, 60_in / second / second}, -40_in, flip, 50_deg,
-                                        0.0),
-                new ScheduleCommand(goalClampTrue),
-                (new Rotate(drivetrain, 180_deg, false, 0.0))->withTimeout(0.8_s),
+                (new Rotate(drivetrain, 0_deg, false, 0.0))->withTimeout(0.8_s),
                 new Ramsete(drivetrain, flip ? &safe_awp_1_blue : &safe_awp_1_red),
-                new TankMotionProfiling(drivetrain, {65_in / second, 60_in / second / second}, -40_in, flip, 50_deg,
-                                        0.0),
-                new ScheduleCommand(goalClampTrue),
-                new Ramsete(drivetrain, flip ? &safe_awp_2_blue : &safe_awp_2_red),
         });
     }
 };
