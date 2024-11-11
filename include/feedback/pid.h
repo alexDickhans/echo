65f5de4fe031eb81a495c6f2237a83d250584465
Alex Dickhans <adickhans@gmail.com>
2024-11-11 16:16:32 -0700

    PID d improvements

--- include/feedback/pid.h
+++ include/feedback/pid.h
@@ -12,7 +12,7 @@ private:
 
 		double error;
 		double totalError = 0.0;
-		double prevError = 0.0;
+		double prevInput = 0.0;
 		double derivitive;
 
 		double integralBound = 3000.0;
@@ -31,7 +31,7 @@ private:
 				this->error = angleDifference(target, input).getValue();
 			}
 
-			this->derivitive = this->error - this->prevError;
+			this->derivitive = - (input - this->prevInput);
 
 			if (abs(error) < integralBound) {
 				totalError += error;
@@ -44,7 +44,7 @@ private:
 
 			this->power = (error * kP) + (derivitive * kD) + (totalError * kI);
 
-			this->prevError = this->error;
+			this->prevInput = input;
 
 			return this->power;
 		}
@@ -132,8 +132,6 @@ public:
 	void setMaxIntegral(double maxIntegral) {
 		this->maxIntegral = maxIntegral;
 	}
-
-
 
 	void reset() {
 		this->prevError = 0;
