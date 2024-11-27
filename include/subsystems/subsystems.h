fa95fa2cb28ef451743b63bda7ccf45e079ef7d7
Alexander Dickhans <adickhans@gmail.com>
2024-11-26 21:30:39 -0700

    fix hang

--- include/subsystems/subsystems.h
+++ include/subsystems/subsystems.h
@@ -114,18 +114,20 @@ inline void subsystemInit() {
     });
 
     barToBarHang =
-            new Sequence({lift->positionCommand(120_deg)->race(drivetrain->hangUp(1.0, 2_in)),
-                          lift->positionCommand(65_deg)->race(drivetrain->hangUp(1.0, 7.7_in)),
-                          lift->positionCommand(120_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.4_s),
-                          lift->positionCommand(120_deg)->race(drivetrain->hangDown(-1.0, 0_in)),
-                          lift->positionCommand(75_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.5_s),
-                          lift->positionCommand(120_deg)->race(drivetrain->hangPctCommand(0.3))->withTimeout(0.5_s)});
+            new Sequence({
+                          lift->positionCommand(55_deg)->race(drivetrain->hangUp(1.0, 7.2_in)),
+                          lift->positionCommand(105_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.6_s),
+                          // lift->positionCommand(115_deg)->race(drivetrain->hangPctCommand(-0.4))->withTimeout(0.5_s),
+                          // lift->positionCommand(115_deg)->race(drivetrain->hangPctCommand(-0.6))->withTimeout(0.2_s),
+                          lift->positionCommand(115_deg)->race(drivetrain->hangDown(-1.0, -2.2_in)),
+                          lift->positionCommand(75_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.2_s),
+                          lift->positionCommand(120_deg)->race(drivetrain->hangPctCommand(1.0))->withTimeout(0.1_s)});
     hang = new Sequence({drivetrain->releaseHang(),
-                         lift->positionCommand(75_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.3_s),
-                         lift->moveToPosition(120_deg)->race(drivetrain->hangPctCommand(0.0)),
-                         lift->positionCommand(120_deg)->race(drivetrain->hangDown(-1.0, 0_in)),
-                         lift->positionCommand(75_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.5_s),
-                         lift->positionCommand(120_deg)->race(drivetrain->hangPctCommand(0.3))->withTimeout(0.5_s),
+                         lift->positionCommand(75_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.2_s),
+                         lift->moveToPosition(125_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.4_s),
+                         lift->positionCommand(125_deg)->race(drivetrain->hangDown(-1.0, -2.2_in)),
+                         lift->positionCommand(75_deg)->race(drivetrain->hangPctCommand(0.0))->withTimeout(0.2_s),
+                         lift->positionCommand(110_deg)->race(drivetrain->hangPctCommand(1.0))->withTimeout(0.1_s),
                          barToBarHang, barToBarHang});
 
     Trigger([]() { return topIntake->ringPresent() && intakeOntoGoal->scheduled(); })
