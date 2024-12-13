15e2db4f3f8cb653453caa155c3e451ecaf461f8
Alexander Dickhans <adickhans@gmail.com>
2024-12-13 10:35:25 -0700

    Awp testing done' git push

--- include/autonomous/awp.h
+++ include/autonomous/awp.h
@@ -20,21 +20,21 @@ public:
         const bool flip = ALLIANCE != RED;
 
         return new Sequence({
-                drivetrain->setNorm(startPose.head<2>(), Eigen::Matrix2f::Identity() * 0.05, startPose.z(), flip),
+                drivetrain->setNorm(startPose.head<2>(), Eigen::Matrix2f::Identity() * 0.02, startPose.z(), flip),
                 new ScheduleCommand(bottomIntake->movePct(0.0)->with(topIntake->pctCommand(0.0))),
-                new TankMotionProfiling(drivetrain, {65_in / second, 100_in / second / second}, 12_in, flip, -180_deg,
+                new TankMotionProfiling(drivetrain, {60_in / second, 100_in / second / second}, 12_in, flip, -180_deg,
                                         0.0),
                 (new Rotate(drivetrain, -90_deg, flip, 0.0))->withTimeout(1.2_s),
                 new ScheduleCommand(bottomIntake->movePct(-1.0)),
-                new TankMotionProfiling(drivetrain, {65_in / second, 100_in / second / second}, -4_in, flip, -90_deg,
+                new TankMotionProfiling(drivetrain, {60_in / second, 100_in / second / second}, -3_in, flip, -90_deg,
                                         0.0),
                 new ScheduleCommand(SharedCommands::scoreAlliance()),
-                (new Rotate(drivetrain, -90_deg, flip, -0.5, false))->withTimeout(0.8_s),
-                new TankMotionProfiling(drivetrain, {65_in / second, 100_in / second / second}, 5_in, flip, -90_deg,
+                (new Rotate(drivetrain, -90_deg, flip, -0.2, false))->withTimeout(0.3_s),
+                new TankMotionProfiling(drivetrain, {60_in / second, 100_in / second / second}, 5_in, flip, -90_deg,
                                         0.0),
                 (new Rotate(drivetrain, 0_deg, flip, 0.0))->withTimeout(0.8_s),
                 new Ramsete(drivetrain, flip ? &safe_awp_2_blue : &safe_awp_2_red),
-                drivetrain->pct(0.15, 0.15)->withTimeout(2.0_s),
+                drivetrain->pct(0.15, 0.15)->withTimeout(3.0_s),
         });
     }
 };
