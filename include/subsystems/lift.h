8d09d22bb8265a31624fac165a52f269b698ee5d
Alexander Dickhans <adickhans@gmail.com>
2025-03-05 13:18:44 -0700

    Format lift.h

--- include/subsystems/lift.h
+++ include/subsystems/lift.h
@@ -10,60 +10,90 @@
 
 class LiftSubsystem : public Subsystem {
 private:
-	pros::MotorGroup motor;
+    pros::MotorGroup motor;
 
-	PID pid;
+    PID pid;
 
-	std::optional<double> voltage;
+    std::optional<double> voltage;
+
+    QTime lastFree = 0.0;
+
 public:
-	explicit LiftSubsystem(const std::initializer_list<int8_t> &motors, const PID& pid) : motor(motors), pid(pid) {
-		motor.set_encoder_units_all(pros::MotorEncoderUnits::rotations);
-		motor.tare_position_all();
-	}
+    explicit LiftSubsystem(const std::initializer_list<int8_t> &motors, const PID &pid) : motor(motors), pid(pid) {
+        motor.set_encoder_units_all(pros::MotorEncoderUnits::rotations);
+        motor.tare_position_all();
+    }
 
-	void periodic() override {
-		if (voltage.has_value()) {
-			motor.move_voltage(voltage.value() * 12000.0);
-		} else {
-			const auto command = pid.update(this->getPosition().Convert(radian));
-			motor.move_voltage(command * 12000.0);
-		}
-	}
+    void periodic() override {
+        if (!voltage.has_value()) {
+            const auto command = pid.update(this->getPosition().Convert(radian));
+            motor.move_voltage(command * 12000.0);
+        }
 
-	void setTarget(Angle angle) {
-		pid.setTarget(angle.Convert(radian));
-		voltage = std::nullopt;
-	}
+        // No-op
+        if (abs(motor.get_current_draw()) < 600) {
+            lastFree = pros::millis() * millisecond;
+        }
+    }
 
-	void setVoltage(double voltage) {
-		this->voltage = voltage;
-	}
+    void setTarget(Angle angle) {
+        pid.setTarget(angle.Convert(radian));
+        voltage = std::nullopt;
+    }
 
-	Angle getPosition() const {
-		return ((motor.get_position(0)) / CONFIG::LIFT_RATIO * revolution);
-	}
+    void setVoltage(double voltage) {
+        this->voltage = voltage;
+        motor.move_voltage(voltage * 12000.0);
+    }
 
-	FunctionalCommand* positionCommand(Angle angle, Angle threshold = 8_deg) {
-		return new FunctionalCommand([]() {}, [this, angle]() { this->setTarget(angle); }, [](bool _) {}, [this, threshold, angle]() { return Qabs(this->getPosition() - angle) < threshold; }, {this});
-	}
+    Angle getPosition() const {
+        return motor.get_position(0) / CONFIG::LIFT_RATIO * revolution;
+    }
 
-	RunCommand* controllerCommand(pros::Controller& controller, pros::controller_analog_e_t channel) {
-		Angle targetPosition = 0.0;
-		return new RunCommand([this, controller, targetPosition, channel]() mutable {
-                    targetPosition = targetPosition + controller.get_analog(channel) / 127.0 * 1.0_deg; targetPosition = std::clamp(targetPosition, 0_deg, 130_deg); this->setTarget(targetPosition); }, {this});
-	}
+    FunctionalCommand *positionCommand(Angle angle, Angle threshold = 8_deg) {
+        return new FunctionalCommand([]() {
+                                     }, [this, angle]() { this->setTarget(angle); }, [](bool _) {
+                                     }, [this, threshold, angle]() {
+                                         return Qabs(this->getPosition() - angle) < threshold;
+                                     }, {this});
+    }
 
-	FunctionalCommand* holdPositionCommand() {
-		return new FunctionalCommand([this]() { this->setTarget(this->getPosition() + copysign(20.0, this->motor.get_actual_velocity()) * degree); }, []() {}, [](bool _) {}, []() { return false; }, {this});
-	}
+    RunCommand *controllerCommand(pros::Controller &controller, pros::controller_analog_e_t channel) {
+        Angle targetPosition = 0.0;
+        return new RunCommand([this, controller, targetPosition, channel]() mutable {
+            targetPosition = targetPosition + controller.get_analog(channel) / 127.0 * 1.0_deg;
+            targetPosition = std::clamp(targetPosition, 0_deg, 130_deg);
+            this->setTarget(targetPosition);
+        }, {this});
+    }
 
-	RunCommand* pctCommand(double voltage) {
-		return new RunCommand([this, voltage]() mutable { this->setVoltage(voltage); }, {this});
-	}
+    FunctionalCommand *holdPositionCommand() {
+        return new FunctionalCommand([this]() {
+                                         this->setTarget(
+                                             this->getPosition() + copysign(20.0, this->motor.get_actual_velocity()) *
+                                             degree);
+                                     }, []() {
+                                     }, [](bool _) {
+                                     }, []() { return false; }, {this});
+    }
 
-	double getTopMotorTemp() const {
-		return this->motor.get_temperature();
-	}
+    RunCommand *pctCommand(double voltage) {
+        return new RunCommand([this, voltage]() mutable { this->setVoltage(voltage); }, {this});
+    }
 
-	~LiftSubsystem() override = default;
+    double getTopMotorTemp() const {
+        return this->motor.get_temperature();
+    }
+
+    bool stalled(QTime duration) const {
+        return pros::millis() * 1_ms - lastFree > duration;
+    }
+
+    FunctionalCommand *zero() {
+        return new FunctionalCommand([this]() { this->setVoltage(1.0); }, []() {
+                                     }, [](bool _) {
+                                     }, [this]() { return this->stalled(300_ms); }, {this});
+    }
+
+    ~LiftSubsystem() override = default;
 };
