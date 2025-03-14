adf0e66e6d4e7db5f83d5cc884231b1bb48dc4cf
Alexander Dickhans <adickhans@gmail.com>
2025-03-13 21:20:20 -0600

    Skills is ready

--- include/subsystems/subsystems.h
+++ include/subsystems/subsystems.h
@@ -123,7 +123,7 @@ inline void initializePathCommands() {
     PathCommands::registerCommand(
         "resetLB", liftSubsystem->positionCommand(10_deg)->withTimeout(1_s)->andThen(liftSubsystem->zero()));
     PathCommands::registerCommand("liftZero", liftSubsystem->positionCommand(6_deg));
-    PathCommands::registerCommand("scoreAllianceStake", liftSubsystem->positionCommand(180_deg, 0.0));
+    PathCommands::registerCommand("scoreAllianceStake", liftSubsystem->positionCommand(185_deg, 0.0));
     PathCommands::registerCommand("outtakeBottom", bottomIntakeSubsystem->pctCommand(-1.0));
     PathCommands::registerCommand("fastLoadLB", fastLoadLB);
     PathCommands::registerCommand("doinkerDown", doinkerDown);
