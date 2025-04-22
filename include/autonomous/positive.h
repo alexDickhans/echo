acad4c1e5c81e6eed3992ad2f8885957829a3506
Alexander Dickhans <adickhans@gmail.com>
2025-04-21 22:19:25 -0600

    Positive auton almost ready

--- include/autonomous/positive.h
+++ include/autonomous/positive.h
@@ -7,7 +7,7 @@ BEZIER_MIRRORED_MP_ASSET(p_4_3);
 class Positive {
 public:
     static Command *p_4() {
-        Eigen::Vector3f startPose{(10.0_in).getValue(), (55.0_in).getValue(), (127_deg).getValue()};
+        Eigen::Vector3f startPose{(-10.0_in).getValue(), (55.0_in).getValue(), (53_deg).getValue()};
 
         drivetrainSubsystem->updateAllianceColor(startPose);
         const bool flip = ALLIANCE != RED;
@@ -24,7 +24,7 @@ public:
     }
 
     static Command *p_1_3() {
-        Eigen::Vector3f startPose{(10.0_in).getValue(), (55.0_in).getValue(), (127_deg).getValue()};
+        Eigen::Vector3f startPose{(-10.0_in).getValue(), (55.0_in).getValue(), (53_deg).getValue()};
 
         drivetrainSubsystem->updateAllianceColor(startPose);
         const bool flip = ALLIANCE != RED;
@@ -34,9 +34,10 @@ public:
             new ScheduleCommand(liftSubsystem->positionCommand(180_deg, 0.0)),
             SharedCommands::driveToAlliance(),
             new Ramsete(drivetrainSubsystem, flip ? &p_4_1_blue : &p_4_1_red),
-            (new Rotate(drivetrainSubsystem, 170_deg, flip))->withTimeout(500_ms),
+            (new Rotate(drivetrainSubsystem, 190_deg, flip))->withTimeout(1000_ms),
             new Ramsete(drivetrainSubsystem, flip ? &p_4_2_blue : &p_4_2_red),
-            SharedCommands::descoreCorner(),
+            SharedCommands::descoreCornerFull(),
+            new ScheduleCommand(intakeWithEject),
             new Ramsete(drivetrainSubsystem, flip ? &p_4_3_blue : &p_4_3_red),
             new ScheduleCommand(liftSubsystem->pctCommand(-0.2)),
         });
