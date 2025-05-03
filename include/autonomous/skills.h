413384835afc058c283dac2776f322eb5741d49d
Alexander Dickhans <adickhans@gmail.com>
2025-05-02 19:19:18 -0600

    Skills worked first try, just minor modifications

--- include/autonomous/skills.h
+++ include/autonomous/skills.h
@@ -43,6 +43,7 @@ public:
             new TankMotionProfiling(drivetrainSubsystem, {50_in/second, 140_in/second/second}, -5.2_in, false, 0.0, 0.0, false),
             new Ramsete(drivetrainSubsystem, &skills_3),
             drivetrainSubsystem->pct(0.3, 0.3)->withTimeout(0.1_s),
+            new ScheduleCommand(intakeNoEject),
             drivetrainSubsystem->pct(0.2, 0.2)->race(
                 SharedCommands::scoreWallStakesWithoutTopIntake()->asProxy())->andThen(
                 new ScheduleCommand(liftSubsystem->positionCommand(155_deg, 0.0))),
