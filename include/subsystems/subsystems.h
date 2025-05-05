b6db3ea7591ee71a1a18887ab8c1b4b66d20058b
Alex Dickhans <adickhans@gmail.com>
2025-05-05 15:20:35 -0500

    Scrim time! lets see liam

--- include/subsystems/subsystems.h
+++ include/subsystems/subsystems.h
@@ -203,8 +203,8 @@ inline void initializeCommands() {
     intakeWithEject = topIntakeWithEject->with(bottomIntakeSubsystem->pctCommand(1.0));
     bottomOuttakeWithEject = topIntakeWithEject->with(bottomIntakeSubsystem->pctCommand(-1.0));
     cornerClearIntakeSequence =
-        topIntakeWithEject->with(bottomIntakeSubsystem->pctCommand(1.0)->withTimeout(10_ms)->andThen(
-            bottomIntakeSubsystem->pctCommand(-1.0)->withTimeout(300_ms)->andThen(
+        topIntakeWithEject->with(bottomIntakeSubsystem->pctCommand(1.0)->withTimeout(100_ms)->andThen(
+            bottomIntakeSubsystem->pctCommand(-1.0)->withTimeout(200_ms)->andThen(
                 bottomIntakeSubsystem->pctCommand(1.0))));
 
     if (topIntakeSubsystem->visionConnected() && AUTON != SKILLS) {
